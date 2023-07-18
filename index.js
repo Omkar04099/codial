cfaad4d152bbf2584ee353dd45ecaea8265c65ce
Waikar <omkar.makarand-waikar@capgemini.com>
2023-07-18 11:41:46 +0530

    setup static files access for layouts and pages

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
 
+app.use(expressLayouts);
+// extract styles and scripts from sub-pages into layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+// set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');    
 //use express router
 app.use('/', require('./routes'));
+
+
 
 app.listen(port, function(err){
     if(err){
